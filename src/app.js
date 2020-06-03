5663b18dc6e5fef084219b727f21526d937b080f
Gabriel <gabriel.santos@logplex.com.br>
2020-06-02 22:52:05 -0300

    Refatoração

--- src/app.js
+++ src/app.js
@@ -1,32 +1,100 @@
 var fs = require("fs");
+let estadoJson = "";
+let cidadeJson ="";
+
+async function init() {
+
+     console.log("                                                ");
+     console.log("****************** Exercício 1 ****************");
+     await CriarArquivosJsonPorEstado();
+}
+
+async function ExecutarTarefas () {
+    
+    estadoJson = await carregarEstadoJson();
+    cidadeJson = await carregarCidadeJson();
+    //console.log("                                                ");
+    console.log("****************** Exercício 2 ****************");
+    console.log( Exer2_QuantidadeCidadesPorEstado('SP'));
+    console.log("                                                ");
+
+    console.log("****************** Exercício 3 ****************");
+    await Exer3_5EstadosComMaisCidades();
+    console.log("                                                ");
+
+    console.log("****************** Exercício 4 ****************");
+    await Exer4_5EstadosComMenosCidades();
+    console.log("                                                ");
+
+    console.log("****************** Exercício 5 ****************");
+    await Exer5_CidadeNomeMaiorPorCadaEstado();
+    console.log("                                                ");
+
+    console.log("****************** Exercício 6 ****************");
+    await Exer6_CidadeNomeMenorPorCadaEstado();
+    console.log("                                                ");
+
+    console.log("****************** Exercício 7 ****************");
+    await Exer7_CidadeNomeMaiorEntreTodosEstados();
+    console.log("                                                ");
+
+    console.log("****************** Exercício 8 ****************");
+    await Exer8_CidadeNomeMenorEntreTodosEstados();
+    console.log("                                                ");
+}
+
+const carregarEstadoJson = async () => {
+    try {
+        const dataEstado = await fs.promises.readFile("../json/Estados.json", "utf-8");
+        return JSON.parse(dataEstado);
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const carregarCidadeJson = async () => {
+    try {
+        const dataCidade = await fs.promises.readFile("../json/Cidades.json", "utf-8");
+        return JSON.parse(dataCidade);
+    } catch (error) {
+        console.log(error)
+    }
+
+}
+
+
 
 // Implementar um método que irá criar um arquivo JSON para cada estado representado no arquivo Estados.json, e o seu conteúdo será um array das cidades pertencentes aquele estado, de acordo com o arquivo Cidades.json. O nome do arquivo deve ser o UF do estado, por exemplo: MG.json.
-const Exer1_CriarJsonCidadePorEstado = () => {
-    var estadoJson = JSON.parse(fs.readFileSync("../json/Estados.json", "utf8"));
-    var cidadeJson = JSON.parse(fs.readFileSync("../json/Cidades.json", "utf8"));
-
-    estadoJson.forEach(element => {
-        const result = cidadeJson.filter((x) => x.Estado === element.ID);
-        if(result){
-            fs.writeFile(`../estados/${element.Sigla}.json`, JSON.stringify(result, null, 4),  'utf8', function(err) {
-                if(err) {
-                    console.log(err);
-                }   
-            }); 
-        }
-    });
-    console.log("Arquivos gerados com sucesso!");
-
+async function CriarArquivosJsonPorEstado() {
+    try {
+        const estado =  await carregarEstadoJson();
+        const cidade =  await carregarCidadeJson();
+        estado.forEach(async element => {
+            const result = cidade.filter((x) => x.Estado === element.ID);
+            if(result) {
+               await fs.promises.writeFile(`../estados/${element.Sigla}.json`, JSON.stringify(result, null, 4),  'utf8' )
+            }
+        });
+        console.log("Arquivos gerados com sucesso!");
+        ExecutarTarefas();
+    } catch (error) {
+        console.log(error)
+    }
 }
 
 //Criar um método que recebe como parâmetro o UF do estado, realize a leitura do arquivo JSON correspondente e retorne a quantidade de cidades daquele estado
 const Exer2_QuantidadeCidadesPorEstado = (estado) => {
-    const ret = fs.readFileSync(`../estados/${estado.toUpperCase()}.json`);
-    if(ret){
-       return JSON.parse(ret).length;
-    } else {
-        resultadoNaoEncontrado();
+    try {
+        const ret = fs.readFileSync(`../estados/${estado.toUpperCase()}.json`);
+        if(ret){
+            return JSON.parse(ret).length;
+        } else {
+            resultadoNaoEncontrado();
+        }        
+    } catch (error) {
+        console.log(error)
     }
+    
 }
 
 
@@ -56,9 +124,8 @@ const Exer4_5EstadosComMenosCidades = () => {
 
 
 //Criar um método que imprima no console um array com a cidade de maior nome de cada estado, seguida de seu UF. Em caso de empate, considerar a ordem alfabética para ordená-los e então retornar o primeiro. Por exemplo: [“Nome da Cidade – UF”, “Nome da Cidade – UF”, ...].
-const Exer5_CidadeNomeMaiorPorCadaEstado  = () => {
-    var estadoJson = JSON.parse(fs.readFileSync("../json/Estados.json", "utf8"));
-    if (estadoJson) {
+const Exer5_CidadeNomeMaiorPorCadaEstado  = async () => {   
+     if (estadoJson) {
         let estados = [];
         estadoJson.forEach(element => {
             const retorno = BuscarTamanhoNomePorCidade(element.Sigla, true);
@@ -74,8 +141,7 @@ const Exer5_CidadeNomeMaiorPorCadaEstado  = () => {
 
 
 // Criar um método que imprima no console um array com a cidade de menor nome de cada estado, seguida de seu UF. Em caso de empate, considerar a ordem alfabética para ordená-los e então retorne o primeiro. Por exemplo: [“Nome da Cidade – UF”, “Nome da Cidade – UF”, ...].
-const Exer6_CidadeNomeMenorPorCadaEstado  = () => {
-    var estadoJson = JSON.parse(fs.readFileSync("../json/Estados.json", "utf8"));
+const Exer6_CidadeNomeMenorPorCadaEstado  = async () => {
     if (estadoJson) {
         let estados = [];
         estadoJson.forEach(element => {
@@ -92,14 +158,11 @@ const Exer6_CidadeNomeMenorPorCadaEstado  = () => {
 
 
 //Criar um método que imprima no console a cidade de maior nome entre todos os estados, seguido do seu UF. Em caso de empate, considerar a ordem alfabética para ordená-los e então retornar o primeiro.
-const Exer7_CidadeNomeMaiorEntreTodosEstados  = () => {
-    var cidadesJson = JSON.parse(fs.readFileSync("../json/Cidades.json", "utf8"));
-    var estadosJson = JSON.parse(fs.readFileSync("../json/Estados.json", "utf8"));
-    if (cidadesJson) {
-
-        const arrayMaiorNome = OrdenarTamanhoMaiorNome(cidadesJson);
+const Exer7_CidadeNomeMaiorEntreTodosEstados = async () => { 
+    if (cidadeJson) {
+        const arrayMaiorNome = OrdenarTamanhoMaiorNome(cidadeJson);
         const cidade =  arrayMaiorNome.slice(0, 1).map(function (item) {
-            let estado = estadosJson.filter(x=> x.ID === item.Estado)
+            let estado = estadoJson.filter(x=> x.ID === item.Estado)
             return `${item.Nome} - ${estado[0].Sigla}`;
         });
        
@@ -112,14 +175,11 @@ const Exer7_CidadeNomeMaiorEntreTodosEstados  = () => {
 
 
 // Criar um método que imprima no console a cidade de menor nome entre todos os estados, seguido do seu UF. Em caso de empate, considerar a ordem alfabética para ordená-los e então retornar o primeiro.
-const Exer8_CidadeNomeMenorEntreTodosEstados  = () => {
-    var cidadesJson = JSON.parse(fs.readFileSync("../json/Cidades.json", "utf8"));
-    var estadosJson = JSON.parse(fs.readFileSync("../json/Estados.json", "utf8"));
-    if (cidadesJson) {
-         
-        const arrayMenorNome = OrdenarTamanhoMenorNome(cidadesJson);
+const Exer8_CidadeNomeMenorEntreTodosEstados  = async () => {
+    if (cidadeJson) {        
+        const arrayMenorNome = OrdenarTamanhoMenorNome(cidadeJson);
         const cidade =  arrayMenorNome.slice(0, 1).map(function (item) {
-            let estado = estadosJson.filter(x=> x.ID === item.Estado)
+            let estado = estadoJson.filter(x=> x.ID === item.Estado)
             return `${item.Nome} - ${estado[0].Sigla}`;
         });
        
@@ -132,7 +192,6 @@ const Exer8_CidadeNomeMenorEntreTodosEstados  = () => {
 
 
 // Funções compartilhadas
-
 
 // Se quantidade positiva mostra as x primeiras, se quantidade negatica mostra x ultimas
 function retornaCidadesPorQuantidade(qtde) {
@@ -159,11 +218,10 @@ function retornaCidadesPorQuantidade(qtde) {
 }
 
 function resultadoNaoEncontrado() {
-    console.log("Resultado Não encontrado");
+    console.log("Resultado não encontrado");
 }
 
 function CriarArrayEstadosQuantidade() {
-    var estadoJson = JSON.parse(fs.readFileSync("../json/Estados.json", "utf8"));
     if (estadoJson) {
         let estados = [];
         estadoJson.forEach(element => {
@@ -213,44 +271,8 @@ function retornarPrimeiraCidadeArray(array, estado) {
 }
 
 
+init();
+
 // Executar Exercicios
-
-//console.log("                                                ");
-//console.log("****************** Exercício 1 ****************");
-//Exer1_CriarJsonCidadePorEstado();
-//console.log("                                                ");
-
-console.log("****************** Exercício 2 ****************");
-console.log(Exer2_QuantidadeCidadesPorEstado('SP'));
-console.log("                                                ");
-
-
-console.log("****************** Exercício 3 ****************");
-Exer3_5EstadosComMaisCidades();
-console.log("                                                ");
-
-
-console.log("****************** Exercício 4 ****************");
-Exer4_5EstadosComMenosCidades();
-console.log("                                                ");
-
-console.log("****************** Exercício 5 ****************");
-Exer5_CidadeNomeMaiorPorCadaEstado();
-console.log("                                                ");
-
-
-console.log("****************** Exercício 6 ****************");
-Exer6_CidadeNomeMenorPorCadaEstado();
-console.log("                                                ");
-
-
-console.log("****************** Exercício 7 ****************");
-Exer7_CidadeNomeMaiorEntreTodosEstados();
-console.log("                                                ");
-
-
-console.log("****************** Exercício 8 ****************");
-Exer8_CidadeNomeMenorEntreTodosEstados();
-console.log("                                                ");
 
 
